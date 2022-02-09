0a07e5935342f37f8ef8368d28012beb9fd74018
Edwardpauldrgst <dreagaster@gmail.com>
2022-02-09 11:03:37 +0100

    web backend feltöltve

---
+++ app/routes/sajatbackend.js
@@ -0,0 +1,165 @@
+const { authJwt } = require("../middleware");
+const controller = require("../controllers/user.controller");
+
+module.exports = function(app) {
+  app.use(function(req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  });
+  app.get('/Ranglista4', (req, res) => {
+    var mysql = require('mysql')
+    var connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'sudoku_ranglista'
+    })
+    
+    connection.connect()
+    
+    connection.query('SELECT * from adatok WHERE adatok.meret = 4 order by adatok.ido asc', function (err, rows, fields) {
+      if (err) throw err
+    
+      console.log(rows)
+      res.send(rows)
+    })
+    
+    connection.end()
+    
+
+  })
+  app.get('/Ranglista5', (req, res) => {
+    var mysql = require('mysql')
+    var connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'sudoku_ranglista'
+    })
+    
+    connection.connect()
+    
+    connection.query('SELECT * from adatok WHERE adatok.meret = 5 order by adatok.ido asc', function (err, rows, fields) {
+      if (err) throw err
+    
+      console.log(rows)
+      res.send(rows)
+    })
+    
+    connection.end()
+    
+
+  })
+  app.get('/Ranglista6', (req, res) => {
+    var mysql = require('mysql')
+    var connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'sudoku_ranglista'
+    })
+    
+    connection.connect()
+    
+    connection.query('SELECT * from adatok WHERE adatok.meret = 6 order by adatok.ido asc', function (err, rows, fields) {
+      if (err) throw err
+    
+      console.log(rows)
+      res.send(rows)
+    })
+    
+    connection.end()
+    
+
+  })
+  app.get('/Ranglista7', (req, res) => {
+    var mysql = require('mysql')
+    var connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'sudoku_ranglista'
+    })
+    
+    connection.connect()
+    
+    connection.query('SELECT * from adatok WHERE adatok.meret = 7 order by adatok.ido asc', function (err, rows, fields) {
+      if (err) throw err
+    
+      console.log(rows)
+      res.send(rows)
+    })
+    
+    connection.end()
+    
+
+  })
+  app.get('/Ranglista8', (req, res) => {
+    var mysql = require('mysql')
+    var connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'sudoku_ranglista'
+    })
+    
+    connection.connect()
+    
+    connection.query('SELECT * from adatok WHERE adatok.meret = 8 order by adatok.ido asc', function (err, rows, fields) {
+      if (err) throw err
+    
+      console.log(rows)
+      res.send(rows)
+    })
+    
+    connection.end()
+    
+
+  })
+  app.get('/Ranglista9', (req, res) => {
+    var mysql = require('mysql')
+    var connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'sudoku_ranglista'
+    })
+    
+    connection.connect()
+    
+    connection.query('SELECT * from adatok WHERE adatok.meret = 9 order by adatok.ido asc', function (err, rows, fields) {
+      if (err) throw err
+    
+      console.log(rows)
+      res.send(rows)
+    })
+    
+    connection.end()
+    
+
+  })
+  app.post('/adattorles', (req, res) => {
+    var mysql = require('mysql')
+    var connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'csevego'
+    })
+    
+    connection.connect()
+    connection.query("DELETE FROM adatok where id=" + req.body.bevitel1, function (err, rows, fields) {
+      if (err) throw err
+    
+      console.log("Sikeres felvitel!")
+      res.send("Sikeres felvitel!")
+    })
+    
+    connection.end()
+    
+
+  })
+};
---
+++ sudoku_ranglista.sql
@@ -0,0 +1,183 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2022. Feb 09. 10:48
+-- Kiszolgáló verziója: 10.4.17-MariaDB
+-- PHP verzió: 8.0.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `sudoku_ranglista`
+--
+CREATE DATABASE IF NOT EXISTS `sudoku_ranglista` DEFAULT CHARACTER SET utf8 COLLATE utf8_hungarian_ci;
+USE `sudoku_ranglista`;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `adatok`
+--
+
+CREATE TABLE `adatok` (
+  `id` int(11) NOT NULL,
+  `nev` varchar(50) COLLATE utf8_hungarian_ci NOT NULL,
+  `ido` int(11) NOT NULL,
+  `meret` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `adatok`
+--
+
+INSERT INTO `adatok` (`id`, `nev`, `ido`, `meret`) VALUES
+(1, 'Robert Trujillo', 120, 6),
+(2, 'James Hetfield', 60, 6),
+(3, 'Lars Ulrich', 240, 4),
+(4, 'Kirk Hammett', 60, 5),
+(5, 'Dave Mustaine', 1500, 9),
+(6, 'Bálint', 120, 4),
+(7, 'Kiverj Elek', 360, 6),
+(8, 'Megrekt Elek', 1200, 9),
+(9, 'Megverj Elek', 1560, 9);
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_hungarian_ci DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `roles`
+--
+
+INSERT INTO `roles` (`id`, `name`, `createdAt`, `updatedAt`) VALUES
+(1, 'user', '2020-08-02 14:57:31', '2020-08-02 14:57:31'),
+(2, 'moderator', '2020-08-02 14:57:31', '2020-08-02 14:57:31'),
+(3, 'admin', '2020-08-02 14:57:31', '2020-08-02 14:57:31');
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) COLLATE utf8_hungarian_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_hungarian_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8_hungarian_ci DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `createdAt`, `updatedAt`) VALUES
+(1, 'user', 'user', '$2a$08$gXM4pyuhZFlC72PeAwxrUOR0uA31/d2PdgnHP35JGV.0bQNiZBatS', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(2, 'mod', 'mod', '$2a$08$gXM4pyuhZFlC72PeAwxrUOR0uA31/d2PdgnHP35JGV.0bQNiZBatS', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(3, 'admin', 'admin', '$2a$08$97Ze1/hXfOX44WdC62Rq8uRkog9HYC1HukRX8eld2ZEKPyenM5v.G', '2020-08-02 15:03:59', '2020-08-02 15:03:59');
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `user_roles`
+--
+
+CREATE TABLE `user_roles` (
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  `roleId` int(11) NOT NULL,
+  `userId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `user_roles`
+--
+
+INSERT INTO `user_roles` (`createdAt`, `updatedAt`, `roleId`, `userId`) VALUES
+('0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 1),
+('2020-08-02 15:04:00', '2020-08-02 15:04:00', 1, 3),
+('0000-00-00 00:00:00', '0000-00-00 00:00:00', 2, 2),
+('0000-00-00 00:00:00', '0000-00-00 00:00:00', 2, 3),
+('2020-08-02 15:04:00', '2020-08-02 15:04:00', 3, 3);
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `adatok`
+--
+ALTER TABLE `adatok`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `user_roles`
+--
+ALTER TABLE `user_roles`
+  ADD PRIMARY KEY (`roleId`,`userId`),
+  ADD KEY `userId` (`userId`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `adatok`
+--
+ALTER TABLE `adatok`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT a táblához `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- Megkötések a kiírt táblákhoz
+--
+
+--
+-- Megkötések a táblához `user_roles`
+--
+ALTER TABLE `user_roles`
+  ADD CONSTRAINT `user_roles_ibfk_1` FOREIGN KEY (`roleId`) REFERENCES `roles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_roles_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
